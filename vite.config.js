de5cddfd4f9fca313c0228fe0c7c78dee307bb46
Alex <oleksandr100204@gmail.com>
2026-01-08 03:23:36 +0100

    fixed: problem with variables; added:finished style for header block

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
-import { defineConfig } from 'vite'
-import path from 'path'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-    },
-  },
-
   css: {
     preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @use "@styles/base/functions" as *;
-          @use "@styles/base/mixins" as *;
-          @use "@styles/base/variables" as *;
-        `,
-      },
+      scss: {},
     },
   },
-})
+});
